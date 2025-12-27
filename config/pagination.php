5baa1665fc97dad48a4ed259f5991f29e08ab3d6
Rully <rully.winarto@gmail.com>
2025-12-28 00:34:14 +0700

    Upload pagination.php via GitZilla Pro

---
+++ config/pagination.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['next_link'] = 'Selanjutnya';
+$config['prev_link'] = 'Sebelumnya';
+$config['first_link'] = 'Awal';
+$config['last_link'] = 'Akhir';
+$config['full_tag_open'] = '<ul class="pagination">';
+$config['full_tag_close'] = '</ul>';
+$config['num_tag_open'] = '<li class="num">';
+$config['num_tag_close'] = '</li>';
+$config['cur_tag_open'] = '<li class="active"><a href="#">';
+$config['cur_tag_close'] = '</a></li>';
+$config['prev_tag_open'] = '<li class="prev">';
+$config['prev_tag_close'] = '</li>';
+$config['next_tag_open'] = '<li class="next">';
+$config['next_tag_close'] = '</li>';
+$config['last_tag_open'] = '<li class="last">';
+$config['last_tag_close'] = '</li>';
+$config['first_tag_open'] = '<li class="first">';
+$config['first_tag_close'] = '</li>';
+
