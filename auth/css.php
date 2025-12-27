42ef8048f63c443361be9290fed0fa3e18bc0b18
Rully <rully.winarto@gmail.com>
2025-12-28 00:42:05 +0700

    Update css.php

---
+++ auth/css.php
@@ -0,0 +1,36 @@
+<style type="text/css">
+	.line {
+	    border-top: 1px solid #ccc;
+	    width: 41%; /* Menyesuaikan panjang garis */
+	    display: inline-block; /* Mengatur agar garis horizontal tidak menempati baris penuh */
+	    vertical-align: middle; /* Menyusun garis horizontal secara vertikal dengan teks */
+	}
+
+	.line-1 {
+	    border-top: 1px solid #ccc;
+	    width: 20%; /* Menyesuaikan panjang garis */
+	    display: inline-block; /* Mengatur agar garis horizontal tidak menempati baris penuh */
+	    vertical-align: middle; /* Menyusun garis horizontal secara vertikal dengan teks */
+	}
+
+	.or-text {
+	    display: inline-block; /* Menyusun teks secara horizontal */
+	    margin: 0 10px; /* Menambahkan jarak antara teks "Atau" dengan garis */
+	}
+
+	.btn-primary {
+	    border-top-left-radius: 0;
+	    border-bottom-left-radius: 0;
+	}
+
+	.input-group-text {
+	    border-top-right-radius: 0;
+	    border-bottom-right-radius: 0;
+	    cursor: pointer;
+	}
+
+	.text-primary {
+	    color: #6f42c1;
+	}
+
+</style>
