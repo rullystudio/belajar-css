1bd8fc20819664a254efac12661446004f223c1a
Rully <rully.winarto@gmail.com>
2025-12-28 00:47:30 +0700

    Hapus alamat/kecamatan.php

--- alamat/kecamatan.php
+++
@@ -1,6 +0,0 @@
-<option value="">Pilih Kecamatan</option>
-<?php 
-	foreach ($hasil as $row) {
-?>
-	<option value="<?php echo $row->subdistrict_id ; ?>"><?php echo $row->subdistrict_name ; ?></option>
-<?php } ?>
