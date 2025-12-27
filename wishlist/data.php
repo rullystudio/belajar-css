1531c6beae3f59256e5da1bb3c6249eb31619cd1
Rully <rully.winarto@gmail.com>
2025-12-28 00:58:21 +0700

    Update data.php

---
+++ wishlist/data.php
@@ -0,0 +1,13 @@
+
+            <div class="table-responsive">
+            	<table class="table table-bordered">
+            		<tr>
+            			<td>
+            				#
+            			</td>
+            			<td>
+            				sdf
+            			</td>
+            		</tr>
+            	</table>
+            </div>
